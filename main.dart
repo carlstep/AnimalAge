784b8d61afcfa5e27c8d6ff3b3cc466a9a99d855
carlstep <>
2021-04-29 20:36:21 +0000

    can now print the calculated value with the key

--- main.dart
+++ main.dart
@@ -3,81 +3,81 @@ import "dart:io";
 
 //var ageRatio = [{dogAgeRatio}, {catAgeRatio}, {bearAgeRatio}, {chickenAgeRatio}, {elephantAgeRatio}, {humanAgeRatio}, {rabbitAgeRatio}]; 
 
-var dogAgeRatio = {'dogTobear': 0.55, 'dogTocat': 0.88, 'dogTochicken': 1.47, 'dogToelephant': 0.31, 'dogTohuman': 0.28, 'dogTorabbit': 2.44};
+var dogAgeRatio = {'Bear': 0.55, 'Cat': 0.88, 'Chicken': 1.47, 'Elephant': 0.31, 'Human': 0.28, 'Rabbit': 2.44}; 
+// dog age ratio Map.
 
-var catAgeRatio = {'catTobear': 0.63, 'catTodog': 1.14, 'catTochicken': 1.67, 'catToelephant': 0.36, 'catTohuman': 0.31, 'catTorabbit': 2.78};
+var catAgeRatio = {'Bear': 0.63, 'Dog': 1.14, 'Chicken': 1.67, 'Elephant': 0.36, 'Human': 0.31, 'Rabbit': 2.78}; 
+// cat age ratio Map.
 
-var bearAgeRatio = {'bearTocat': 1.6, 'bearTodog': 1.82, 'bearTochicken': 2.67, 'bearToelephant': 0.57, 'bearTohuman': 0.5, 'bearTorabbit': 4.44};
+var bearAgeRatio = {'Bear': 1.6, 'Dog': 1.82, 'Chicken': 2.67, 'Elephant': 0.57, 'Human': 0.5, 'Rabbit': 4.44}; 
+// bear age ratio Map.
 
-var chickenAgeRatio = {'chickenTobear': 0.38, 'chickenTodog': 0.68, 'chickenTocat': 0.6, 'chickenToelephant': 0.21, 'chickenTohuman': 0.19, 'chickenTorabbit': 1.67};
+var chickenAgeRatio = {'Bear': 0.38, 'Dog': 0.68, 'Cat': 0.6, 'Elephant': 0.21, 'Human': 0.19, 'Rabbit': 1.67}; 
+// chicken age ratio Map.
 
-var elephantAgeRatio = {'elephantTobear': 1.75, 'elephantTodog': 3.18, 'elephantTochicken': 4.67, 'elephantTocat': 2.8, 'elephantTohuman': 0.88, 'elephantTorabbit': 7.78};
+var elephantAgeRatio = {'Bear': 1.75, 'Dog': 3.18, 'Chicken': 4.67, 'Cat': 2.8, 'Human': 0.88, 'Rabbit': 7.78}; 
+// elephant age ratio Map.
 
-var humanAgeRatio = {'humanTobear': 2, 'humanTodog': 3.64, 'humanTochicken': 5.33, 'humanTocat': 3.2, 'humanToelephant': 1.14, 'humanTorabbit': 8.89};
+var humanAgeRatio = {'Bear': 2, 'Dog': 3.64, 'Chicken': 5.33, 'Cat': 3.2, 'Elephant': 1.14, 'Rabbit': 8.89}; 
+// human age ratio Map.
 
-var rabbitAgeRatio = {'rabbitTobear': 0.23, 'rabbitTodog': 0.41, 'rabbitTochicken': 0.6, 'rabbitTocat': 0.36, 'rabbitToelephant': 0.13, 'rabbitTohuman': 0.11};
+var rabbitAgeRatio = {'Bear': 0.23, 'Dog': 0.41, 'Chicken': 0.6, 'Cat': 0.36, 'Elephant': 0.13, 'Human': 0.11}; 
+// rabbit age ratio Map.
 
 int age(String promptAge) {
     print(promptAge);
     int answerAge = int.parse(stdin.readLineSync());
-    return answerAge;
+    return answerAge; 
+    // the prompt function for userInput <int> 'promptAge'
 }
 
 String prompt(String promptAnimal) {
     print(promptAnimal);
     String answerAnimal = stdin.readLineSync();
-    return answerAnimal;
+    return answerAnimal; 
+    // the prompt function for userInput <String> 'promptAnimal'
 }
 
 void main() {
-  String animalName = prompt('choose from - dog, bear, cat, chicken, elephant, human, rabbit: ');
+  String animalName = prompt('choose from - dog, bear, cat, chicken, elephant, human, rabbit: '); 
+  // calls the promptAnimal function to get the animal choice.
   
   int howOld = age("What is the age of the animal? ");
-  //int animalAge = int.parse(stdin.readLineSync());
-
-var selectedValues = [];
-//var selectedKey = [];
+  // calls the promptAnimal function to get the animal choice.
 
   if(animalName == "dog") {
-    selectedValues = dogAgeRatio.values.toList();
-    //selectedKey = dogAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${dogAgeRatio.values}");
+    dogAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+        // called if userInput is 'dog', takes each k:v entry, assigns to 'element' and prints the key and value(value * howOld). 
+
     } else if(animalName == "cat") {
-    selectedValues = catAgeRatio.values.toList();
-    //selectedKey = catAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${catAgeRatio.values}");
+    catAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+    
     } else if (animalName == "elephant"){
-    selectedValues = elephantAgeRatio.values.toList();
-    //selectedKey = elephantAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${elephantAgeRatio.values}");
+    elephantAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+
     } else if (animalName == "human") {
-    selectedValues = humanAgeRatio.values.toList();
-    //selectedKey = humanAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${humanAgeRatio.values}");
+    humanAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+
     } else if (animalName == "chicken") {
-    selectedValues = chickenAgeRatio.values.toList();
-    //selectedKey = chickenAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${chickenAgeRatio.values}");
+    chickenAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+
     } else if (animalName == "rabbit") {
-    selectedValues = rabbitAgeRatio.values.toList();
-    //selectedKey = rabbitAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${rabbitAgeRatio.values}");
+    rabbitAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+    
     } else {
-    selectedValues = bearAgeRatio.values.toList();
-    //selectedKey = bearAgeRatio.keys.toList();
-    print("these are the relatve animal ages: ${bearAgeRatio.values}");
+    bearAgeRatio.entries.forEach((element) =>
+        print('${element.key} => ${element.value * howOld}'));
+    }
+
 }
-
-//var selectedList = dogAgeRatio.values.toList();
-
-for (double item in selectedValues) {
-    print(item * howOld);
-    //print(dogAgeRatio.key);
-}
-
-//double dogBearAge = dogRatioList.reduce((value, element) =>  * howOld;
-//print(dogRatioList.forEach * howOld);
 
 }
 // DONE - how to take the ratio values of the animal and multiply by a given age 
-// TODO - how to asign the key to the output values or (return key:value, value * howOld)?
+// DONE - how to asign the key to the output values or (return key:value, value * howOld)?
+// TODO - can the 
