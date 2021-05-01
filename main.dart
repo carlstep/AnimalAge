b9f9080f5bf160f2c90fb390702ecf0924ca9ca9
carlstep <>
2021-05-01 06:58:00 +0000

    updated the text for the print statement

--- main.dart
+++ main.dart
@@ -1,28 +1,21 @@
 import "dart:math";
 import "dart:io";
 
-//var ageRatio = [{dog}, {cat}, {bear}, {chicken}, {elephant}, {human}, {rabbit}]; 
+//var ageRatio = [{dog}, {cat}, {bear}, {chicken}, {elephant}, {human}, {rabbit}]; // List of Map
 
-var dog = {'Bear': 0.55, 'Cat': 0.88, 'Chicken': 1.47, 'Elephant': 0.31, 'Human': 0.28, 'Rabbit': 2.44}; 
-// dog age ratio Map.
+var dog = {'Bear': 0.55, 'Cat': 0.88, 'Chicken': 1.47, 'Elephant': 0.31, 'Human': 0.28, 'Rabbit': 2.44}; // dog age ratio Map.
 
-var cat = {'Bear': 0.63, 'Dog': 1.14, 'Chicken': 1.67, 'Elephant': 0.36, 'Human': 0.31, 'Rabbit': 2.78}; 
-// cat age ratio Map.
+var cat = {'Bear': 0.63, 'Dog': 1.14, 'Chicken': 1.67, 'Elephant': 0.36, 'Human': 0.31, 'Rabbit': 2.78}; // cat age ratio Map.
 
-var bear = {'Bear': 1.6, 'Dog': 1.82, 'Chicken': 2.67, 'Elephant': 0.57, 'Human': 0.5, 'Rabbit': 4.44}; 
-// bear age ratio Map.
+var bear = {'Bear': 1.6, 'Dog': 1.82, 'Chicken': 2.67, 'Elephant': 0.57, 'Human': 0.5, 'Rabbit': 4.44}; // bear age ratio Map.
 
-var chicken = {'Bear': 0.38, 'Dog': 0.68, 'Cat': 0.6, 'Elephant': 0.21, 'Human': 0.19, 'Rabbit': 1.67}; 
-// chicken age ratio Map.
+var chicken = {'Bear': 0.38, 'Dog': 0.68, 'Cat': 0.6, 'Elephant': 0.21, 'Human': 0.19, 'Rabbit': 1.67}; // chicken age ratio Map.
 
-var elephant = {'Bear': 1.75, 'Dog': 3.18, 'Chicken': 4.67, 'Cat': 2.8, 'Human': 0.88, 'Rabbit': 7.78}; 
-// elephant age ratio Map.
+var elephant = {'Bear': 1.75, 'Dog': 3.18, 'Chicken': 4.67, 'Cat': 2.8, 'Human': 0.88, 'Rabbit': 7.78}; // elephant age ratio Map.
 
-var human = {'Bear': 2, 'Dog': 3.64, 'Chicken': 5.33, 'Cat': 3.2, 'Elephant': 1.14, 'Rabbit': 8.89}; 
-// human age ratio Map.
+var human = {'Bear': 2, 'Dog': 3.64, 'Chicken': 5.33, 'Cat': 3.2, 'Elephant': 1.14, 'Rabbit': 8.89}; // human age ratio Map.
 
-var rabbit = {'Bear': 0.23, 'Dog': 0.41, 'Chicken': 0.6, 'Cat': 0.36, 'Elephant': 0.13, 'Human': 0.11}; 
-// rabbit age ratio Map.
+var rabbit = {'Bear': 0.23, 'Dog': 0.41, 'Chicken': 0.6, 'Cat': 0.36, 'Elephant': 0.13, 'Human': 0.11}; // rabbit age ratio Map.
 
 int age(String promptAge) {
     print(promptAge);
@@ -40,39 +33,38 @@ String prompt(String promptAnimal) {
 
 void main() {
   String animalName = prompt('choose from - dog, bear, cat, chicken, elephant, human, rabbit: '); 
-  // calls the promptAnimal function to get the animal choice.
+  // calls the promptAnimal function, assigns userInput - animal choice - to animalName.
   
   int howOld = age("What is the age of the animal? ");
-  // calls the promptAnimal function to get the animal choice.
+  // calls the promptAnimal function, assigns userInput - age - to howOld.
 
   if(animalName == "dog") {
     dog.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
-        // called if userInput is 'dog', takes each k:v entry, assigns to 'element' and prints the key and value(value * howOld). 
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}')); // called if userInput is 'dog', takes each k:v entry, assigns to 'element' and prints the key and value(value * howOld). 
 
     } else if(animalName == "cat") {
     cat.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
     
     } else if (animalName == "elephant"){
     elephant.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
 
     } else if (animalName == "human") {
     human.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
 
     } else if (animalName == "chicken") {
     chicken.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
 
     } else if (animalName == "rabbit") {
     rabbit.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
     
     } else {
     bear.entries.forEach((element) =>
-        print('${element.key} => ${(element.value * howOld).toStringAsFixed(2)}'));
+        print('The relative age for a ${element.key} is approx ${(element.value * howOld).toStringAsFixed(2)}'));
     }
 
 }
