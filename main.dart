bbf0075db41c5a63e08e90e69187498c8f6e3713
carlstep <>
2021-05-03 19:36:49 +0000

    cleaned up comments

--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
 import "dart:math";
 import "dart:io";
 
-// a single Map will make it easier to add new animalAgeRatio to the list.
 var humanMap = {
     'bear': 2,
     'chicken': 5.33, 
@@ -11,11 +10,6 @@ var humanMap = {
     'human': 1,
     'rabbit': 8.89,
     }; // human age ratio Map.
-
-// ageRatioCalc examples - 
-// - (animalYear(cat - 8) / human(3.2) = 2.5) * animal(dog - 3.64) = 9.1 (catAge=8, humanAge=2.5, dogAge=9.1) 
-// - (animalYear(human - 10) / human(1) = 10) * animal(dog - 3.64) = 36.4 (humanAge=10, humanAge=10, dogAge=36.4)
-// - (animalYear(elephant - 10) / human(1) = 8.77) * animal(dog - 3.64) = 31.9 (elephantAge=10, humanAge=8.77, dogAge=31.9)
 
 int age(String promptAge) {
     print(promptAge);
@@ -35,7 +29,7 @@ double ratio(String ageRatio) {
     print(ageRatio);
     double answerRatio = double.parse(stdin.readLineSync());
     return answerRatio; 
-    // the prompt function for userInput <double> 'ratio'
+    // the prompt function for userInput <double> 'animalRatio'
 }
 
 void ageCalc() {
@@ -43,11 +37,6 @@ void ageCalc() {
     humanMap.forEach((key, val) {
         print("- a ${key}'s age would be approx. ${(howOld * val).toStringAsFixed(1)} ");
 }); // function to return a list of animals in the map + howOld userInput.
-}
-
-void addAnimal() {
-    // add animal name key
-    // add animal ageRatio value
 }
 
 int howOld = 0;
@@ -69,14 +58,10 @@ void main() {
         newAnimal = prompt("Type '$animalName' again to add to the list.");
         animalRatio = ratio("Enter a number in decimal format, eg 2.3: ");
         print("$newAnimal and $animalRatio");
-        humanMap.putIfAbsent(newAnimal, () => animalRatio);
+        humanMap.putIfAbsent(newAnimal, () => animalRatio); // puts the new animal:ratio (k,v) pair into the humanMap.
         print(humanMap);
         main();
         }
-  
-    
-    //${(element.value * howOld).toStringAsFixed(2)}
-
 }
 
 // TODO - create function to call calc (how many calculations/functions are needed 
